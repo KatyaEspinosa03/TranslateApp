a53ce92617ea27f0add0cd18e72e3ebcf706912a
KatyaEspinosa03 <katyamiranda03@hotmail.com>
2023-11-12 18:09:04 -0600

    Added a dispatchqueue to reload the data from the tableView

---
+++ TranslateApp/SecondTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SecondTableViewCell.swift
+//  TranslateApp
+//
+//  Created by Katya Miranda on 10/11/23.
+//
+
+import UIKit
+
+class SecondTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
