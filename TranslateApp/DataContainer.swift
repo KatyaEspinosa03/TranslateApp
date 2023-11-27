d1b2e63a7e836567ecfa9dde208694e2749ce883
KatyaEspinosa03 <katyamiranda03@hotmail.com>
2023-11-26 18:59:13 -0600

    Refactored code

---
+++ TranslateApp/DataContainer.swift
@@ -0,0 +1,8 @@
+//
+//  DataContainer.swift
+//  TranslateApp
+//
+//  Created by Katya Miranda on 26/11/23.
+//
+
+import Foundation
---
+++ TranslateApp/TranslateViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TranslateViewModel.swift
+//  TranslateApp
+//
+//  Created by Katya Miranda on 26/11/23.
+//
+
+import Foundation
